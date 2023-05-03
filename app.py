400e6094e6ad8a0ff7ca49d2fc1f3799a06d626c
savitri patil <114324234+savipatil97@users.noreply.github.com>
2023-05-03 12:42:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+app.config['SERVER_NAME'] = 'localhost:5000'
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World!'
+
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(debug=True)
